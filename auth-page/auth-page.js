fcb9b9c48e8a6c2b2d90d687414c76e5f55af0ee
‘AMAYA <aamaya547@gmail.com>
2022-05-18 14:37:50 -0700

    login is working, leads to main page

--- auth-page/auth-page.js
+++ auth-page/auth-page.js
@@ -1,14 +1,25 @@
-import { signUpUser } from '../fetch-utils.js';
+import { signUpUser, signInUser } from '../fetch-utils.js';
 
 const signUpForm = document.getElementById('sign-up');
+const signInForm = document.getElementById('sign-in');
 
 // redirectIfLoggedIn();
 
 signUpForm.addEventListener('submit', async (e) => {
     e.preventDefault();
     const data = new FormData(signUpForm);
-    console.log({ email: data.get('email'), password: data.get('password') });
+    // console.log({ email: data.get('email'), password: data.get('password') });
     const user = await signUpUser(data.get('email'), data.get('password'));
+    if (user) {
+        location.replace('/');
+    }
+});
+
+signInForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const data = new FormData(signInForm);
+    // console.log({ email: data.get('email'), password: data.get('password') });
+    const user = await signInUser(data.get('email'), data.get('password'));
     if (user) {
         location.replace('/');
     }
